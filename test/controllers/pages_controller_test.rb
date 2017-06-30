0f5575504c562cf0c088d1c13ffda203e90d9cc5
Powakedown <abriko@yahoo.fr>
2017-06-30 11:57:40 +0200

    Add test rooting on the home page

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,4 +4,19 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @base_title = "Parentheze, la nouvelle vie des parents"
+  end
+
+  test "should get root_path" do
+    get root_path
+    assert_response :success
+    assert_select "title", "Accueil | #{@base_title}"
+  end
+
+  test "should get home" do
+    get home_url
+    assert_response :success
+    assert_select "title", "Accueil | #{@base_title}"
+  end
 end
